ddaea2692cdffb3451f9911e827f7541e462a43f
josh6512 <yhoshoa144@gmail.com>
2023-09-21 16:12:58 +0300

    server port change

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/", async(req, res) => {
 });
 
 // Start the server
-const PORT = 5501;
+const PORT = process.env.PORT || 5501;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
