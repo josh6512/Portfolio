9f0dc7584a0306f77a2f16c7d22823aa40ab3c58
josh6512 <yhoshoa144@gmail.com>
2023-09-21 12:11:18 +0300

    server

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 
 const navbarToggle = document.getElementById('navbar-toggle');
 const navbarLinks = document.getElementById('nav-list');
-
-// Add click event listener to the toggle button
 navbarToggle.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
+});
+
+/////
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contactForm");
+
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(contactForm);
+
+        // Create an object from the form data
+        const formDataObj = {};
+        formData.forEach((value, key) => {
+            formDataObj[key] = value;
+        });
+
+        // Send the data to the server using a POST request
+        fetch("/", {
+            method: "POST",
+            body: JSON.stringify(formDataObj),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    alert("Email sent successfully!");
+                } else {
+                    alert("Email sending failed.");
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    });
 });
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files (e.g., HTML, CSS, and client-side JavaScript)
+app.use(express.static(__dirname));
+
+// Handle form submission
+app.post("/", async(req, res) => {
+ const { name, email, message } = req.body;
+
+    // Create a nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: "Gmail", // e.g., "Gmail"
+        auth: {
+            user: "yhoshoa144@gmail.com",
+            pass: "nzvfncwtwkfbosku"
+        },
+        tls: {
+        rejectUnauthorized: false, // Ignore SSL certificate errors
+    },
+    });
+
+    // Email data
+    const mailOptions = {
+        from: email,
+        to: "yhoshoa144@gmail.com",
+        subject: `new mail fropm protfolio `,
+        text: `Name: ${name}\n Email: ${email}\n Message: ${message} `,
+    };
+
+    // Send email
+    await  transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error("Error:", error);
+            res.status(500).json({ success: false });
+        } else {
+            console.log("Email sent:", info.response);
+            res.json({ success: true });
+        }
+    });
+});
+
+// Start the server
+const PORT = 5501;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
